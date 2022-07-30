2dc3a721120b5f575d2152ee5bea2a154f5ed93b
Mohammed AbuIbaid <m76eef@gmail.com>
2022-07-30 16:05:50 +0300

    added exceptions

--- Product.API/Service/ProductService.cs
+++ Product.API/Service/ProductService.cs
@@ -1,6 +1,8 @@
 ﻿using Product.API.Data;
 using Product.API.Dtos;
+using Product.API.Exceptions;
 using Product.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -29,11 +31,19 @@ namespace Product.API.Service
 
         public int Create(CreateProductDto dto)
         {
+            var NameIsExist = _db.Products.Any(x => x.Name == dto.Name);
+            if (NameIsExist)
+            {
+                throw new DuplicatedProductNameException();
+            }
+            if(dto.Cost > dto.Price)
+            {
+                throw new CostHigherThanPriceException();
+            }
             var product = new ProductEntity();
             product.Name = dto.Name;
             product.Cost = dto.Cost;
             product.Price = dto.Price;
-
             _db.Products.Add(product);
             _db.SaveChanges();
             return product.Id;
@@ -41,13 +51,20 @@ namespace Product.API.Service
 
         public int Update(UpdateProductDto dto)
         {
-            var product = _db.Products.Find(dto.Id);
-            product.Name = dto.Name;
-            product.Cost = dto.Cost;
-            product.Price = dto.Price;
-            _db.Products.Update(product);
-            _db.SaveChanges();
-            return product.Id;
+            try
+            {
+                var product = _db.Products.Find(dto.Id);
+                product.Name = dto.Name;
+                product.Cost = dto.Cost;
+                product.Price = dto.Price;
+                _db.Products.Update(product);
+                _db.SaveChanges();
+                return product.Id;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
         }
 
         public void Delete(int Id)
